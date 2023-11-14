35691e555c6eda09736983c79e3eca129989994c
adytia01 <prayogaadyt2@gmail.com>
2023-11-14 11:39:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>RESTO PNG</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+</ul>
+</nav>
+</header>
+
+<section class="menu">
+    <h2>MENU MAKANAN RESTO PNG</h2>
+    <div class="menu-item">
+        <img src="img/bakso.jpg" alt="menu 1">
+        <h3>bakso granat</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 15.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/soto.jpg" alt="menu 2">
+        <h3>soto</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 10.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Sate.jpg" alt="menu 2">
+        <h3>Sate</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 25.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Nasi Goreng.jpg" alt="menu 2">
+        <h3>Nasi Goreng</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 20.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Gudeg.jpg" alt="menu 2">
+        <h3>Gudeg</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 30.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Gulai Kambing.jpg" alt="menu 2">
+        <h3>Gulai Kambing</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 45.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Mie ayam.jpg" alt="menu 2">
+        <h3>Mie ayam</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 10.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Nasi Pecel.jpg" alt="menu 2">
+        <h3>Nasi Pecel</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 12.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Nasi Rames.jpg" alt="menu 2">
+        <h3>Nasi Rames</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 18.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Ayam Bakar.jpg" alt="menu 2">
+        <h3>Ayam Bakar</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 14.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Ayam Panggang.jpg" alt="menu 2">
+        <h3>Ayam Panggang</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 15.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Ayam Geprek.jpg" alt="menu 2">
+        <h3>Ayam Geprek</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 8.000</p>
+         <button class="order-button">pesan</button>
+</div>
+    <h2>MENU MINUMAN RESTO PNG</h2>
+    <div class="menu-item">
+        <img src="img/Es Teh.jpg" alt="menu 1">
+        <h3>Es Teh</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 4.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Es Jeruk.jpg" alt="menu 1">
+        <h3>Es Jeruk</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 5.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Es Buah.jpg" alt="menu 1">
+        <h3>Es Buah</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 8.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Mangga.jpg" alt="menu 1">
+        <h3>Jus Mangga</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 10.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Alpukad.jpg" alt="menu 1">
+        <h3>Jus Alpukad</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 7.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Jambu.jpg" alt="menu 1">
+        <h3>Jus Jambu</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 6.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Semangka.jpg" alt="menu 1">
+        <h3>Jus Semangka</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 8.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Strawberi.jpg" alt="menu 1">
+        <h3>Jus Strawberi</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 12.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Nanas.jpg" alt="menu 1">
+        <h3>Jus Nanas</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 10.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Anggur.jpg" alt="menu 1">
+        <h3>Jus Anggur</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 10.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Apel.jpg" alt="menu 1">
+        <h3>Jus Apel</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 7.000</p>
+         <button class="order-button">pesan</button>
+</div>
+<div class="menu-item">
+        <img src="img/Jus Buah Naga.jpg" alt="menu 1">
+        <h3>Jus Buah Naga</h3>
+        <p>
+            deskripsi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : RP. 8.000</p>
+         <button class="order-button">pesan</button>
+</div>
+</section>
+
+<section class="contact">
+    <h2>hubungi kami</h2>
+    <p>jika ada pertanyaan, saran atau kritik hubungi kami di:</p>
+    <p>wa 087753159692</p>
+    <p>email : prayogaadyt2@gmail.com</p>
+</section>
+
+<footer>
+    <p>&copy; 2023 restoran enak .hak cipta di lindungi</p>
+</footer>
+</body>
+</html>
